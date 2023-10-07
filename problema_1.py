617d8f9fb0bdc0c2ad4fbb0ef915cc8de33eb7ae
Prisacari Arina <dunea81@mail.ru>
2023-10-07 22:29:41 +0300

    am rezolvat problemele

---
+++ problema_1.py
@@ -0,0 +1,53 @@
+print('---problema 1---')
+a='*'
+print('A-----------')
+print(a)
+print(a,a)
+print(a,a,a,)
+print(a,a,a,a)
+print("B--------")
+print( a,a,a,a)
+print('',a,a,a,)
+print('' ,a,a)
+print( '', a)
+print('C---------')
+print(a,a,a,a)
+print('',a,a,a)
+print(' ', a,a)
+print('  ', a)
+print('D-------')
+print('  ',a )
+print(' ',a,a)
+print('',a,a,a)
+print(a,a,a,a)
+
+print('---problema 2---')
+print('n')
+print('n+2 n+2')
+print('n+4 n+4 n+4')
+print('n+6 n+6 n+6 n+6')
+
+print('---problema 3---')
+a=1
+b=2
+c=3
+d=4
+print('a=',a ,sep='')
+print('b=',b ,sep='')
+print('c=',c ,sep='')
+print('d=',d ,sep='')
+print('in saptamana',(a+b+c+d)*7)
+
+print('---problema 4---')
+v=10
+g=2*v+8
+i=5*v+80
+print('v=', v, sep='')
+print('g=', g, sep='')
+print('i=', i, sep='') 
+
+print('---problema 5---')
+ion=57
+vasile=ion-2, ion-1, ion, ion+1, ion+2
+print('ion spune', ion)
+print('vasile raspunde', vasile)
